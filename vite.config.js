a91f60713d43e094a186db349727cad264324740
dwakshar <akshar2.dnd@gmail.com>
2026-04-12 00:58:36 +0530

    feat: deploymwnt

--- vite.config.js
+++ vite.config.js
@@ -1,30 +1,9 @@
 import react from '@vitejs/plugin-react';
-import { dirname, resolve } from 'path';
-import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    lib: {
-      entry: resolve(__dirname, 'src/Widget.jsx'),
-      name: 'Humora',
-      formats: ['iife'],
-      fileName: () => 'humora.min.js',
-    },
-    rollupOptions: {
-      external: [],
-      output: {
-        inlineDynamicImports: true,
-        assetFileNames: (assetInfo) => {
-          if (assetInfo.name === 'style.css') return 'humora.css';
-          return assetInfo.name;
-        },
-      },
-    },
     outDir: 'dist',
-    cssCodeSplit: false,
   },
 });
